70712f997c2eb05aa704afcfbfb4afd54bf8d2e9
khokenneth31 <khokenneth26@gmail.com>
2024-02-14 09:13:04 +0800

    first repo

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const createGallaryItems = galleryItems
+  .map(({ original, preview, description }) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-src="${original}" alt="${description}" />
+      </a>
+    </li>`
+  ).join("");
+
+galleryContainer.insertAdjacentHTML('beforeend', createGallaryItems);
+
+const galleryHandler = new SimpleLightbox('ul.gallery a', { 
+  captionsData: 'alt',
+  captionDelay: 250
+});
+galleryHandler.on("show.simplelightbox");
